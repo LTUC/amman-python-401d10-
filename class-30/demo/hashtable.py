e3e1cd627409f77948b4d13e60f7ac7ab435c5e3
roaaabualeeqa <roaa.abualeeqa@gmail.com>
2023-06-25 10:47:11 +0000

    class-30 done

---
+++ class-30/demo/hashtable.py
@@ -0,0 +1,39 @@
+from linked_list import LinkedList
+
+class HashTable():
+    def __init__(self,size=3):
+        self.size = size
+        self.map = [None]*size
+
+    def custom_hash(self, key):
+        sum_of_asccii = 0
+        for ch in key:
+            asccii_of_ch = ord(ch)
+            sum_of_asccii += asccii_of_ch
+        temp = sum_of_asccii*599
+        indx = temp%self.size
+        return indx
+    
+    def set(self, key, value):
+        hashed_key = self.custom_hash(key)
+        if not self.map[hashed_key]: # if the Bucket is empty
+            self.map[hashed_key] = [key,value]
+        else: # collision happeded
+            if isinstance(self.map[hashed_key], LinkedList):
+                self.map[hashed_key].add([key,value])
+            else: # if the bucket contains one pair only
+                chain = LinkedList()
+                exsiting_pair = self.map[hashed_key]
+                new_pair = [key, value]
+                self.map[hashed_key] = chain
+                chain.add(exsiting_pair)
+                chain.add(new_pair)
+
+
+
+
+
+
+
+
+        
---
+++ class-30/demo/main.py
@@ -0,0 +1,13 @@
+from hashtable import HashTable
+
+hashtable1 = HashTable()
+
+hashtable1.set("A", 70)
+hashtable1.set("B", 90)
+hashtable1.set("C", 80)
+hashtable1.set("E", 60)
+
+print(hashtable1.map[0])
+print(hashtable1.map[1])
+print(hashtable1.map[2])
+
